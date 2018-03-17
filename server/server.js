4f7cd217f52b27616931a846c40738f89a1e82a8
Andre <andreducharme@andre-ducharmes-macbook.local>
2018-03-17 13:50:49 -0600

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 const express = require('express');
 const bodyParser = require('body-parser'); //keep a space between library imports and local file imports.
 
@@ -27,6 +29,25 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	 if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+		}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+		return res.status(404).send();
+		console.log('Todo not found!');
+		}
+		res.status(200).send({todo});
+		console.log(`Todo Found: ${todo}`);
+	}).catch((e) => {
+		res.status(400).send();
+	})
 });
 
 
