e3029a8ee319b46c1d6d013673fbd0585752dad3
Andre <andreducharme@andre-ducharmes-macbook.local>
2018-03-20 21:39:42 -0600

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -122,7 +122,7 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-//POST /users/login {email, password}
+
 
 app.post('/users/login', (req, res) => {
 	let body = _.pick(req.body, ['email', 'password']);
@@ -140,5 +140,22 @@ app.post('/users/login', (req, res) => {
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
+
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
+	});
+});
+
+
+
+
+
+
+
+
 
 module.exports = {app};
